2a6fb70da7a134fa58d762b03372003391a060f6
João Pedro Camargo Pinheiro <joaoppinheiro2005@gmail.com>
2025-05-27 14:32:14 -0300

    feat: add CarService

---
+++ src/main/java/com/mecmanager/service/CarService.java
@@ -0,0 +1,36 @@
+package com.mecmanager.service;
+
+import com.mecmanager.entity.Car;
+import com.mecmanager.repository.CarRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarService {
+
+    private final CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Car saveCar(Car Car) {
+        return carRepository.save(Car);
+    }
+
+    public List<Car> findAllCars() {
+        return carRepository.findAll();
+    }
+
+    public Optional<Car> findCarById(Long id) {
+        return carRepository.findById(id);
+    }
+
+    public Optional<Car> findCarByLicensePlate(String licensePlate) {
+        return carRepository.findByLicensePlate(licensePlate);
+    }
+
+
+}
